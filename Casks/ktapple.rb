ab4cd54012f5d4101d36c9b3cf16a4bebe499c3d
Yusuke Harada <49523406+m96-chan@users.noreply.github.com>
2026-02-27 08:32:38 +0000

    Update KTApple to 1.3.0

--- Casks/ktapple.rb
+++ Casks/ktapple.rb
@@ -1,6 +1,6 @@
 cask "ktapple" do
-  version "1.2.0"
-  sha256 "6af38ac23fe9e282dd35014122f1a98a86b6ddf2608910df52cadce204a1c7fc"
+  version "1.3.0"
+  sha256 "f63ea929858744f7b69720c07979ed11d0f06dfb0d06802336f00837fb71506b"
 
   url "https://github.com/m96-chan/KTApple/releases/download/v#{version}/KTApple-#{version}-arm64.dmg"
   name "KTApple"
